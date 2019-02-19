9d67938d4849e88b0e671183b45ae265096449e7
Paul Chubatyy <xobb@citylance.biz>
2019-02-18 20:47:21 -0800

    Add a good looking weather forecast

--- app/api.py
+++ app/api.py
@@ -5,23 +5,48 @@ import datetime as dt
 import pytz
 
 
-def format_datetime(value, format='medium'):
-    if format == 'full':
-        format = "EEEE, d. MMMM y 'at' HH:mm"
-    elif format == 'medium':
-        format = "EE dd.MM.y HH:mm"
-    return babel.dates.format_datetime(value, format)
-
-
-api.jinja_env.filters['datetime'] = format_datetime
-
-api = responder.API()
 env.read_envfile()
+
+api = responder.API(
+    debug=env.bool('DEBUG', default=False),
+    secret_key=env.str('SECRET_KEY')
+)
+
+local_tz = pytz.timezone(env.str('TIME_ZONE', default='America/Los_Angeles'))
+timeformat = env.str('TIME_FORMAT', default='%H:%M')
+dateformat = env.str('DATE_FORMAT', default='%a, %b %-d')
+datetimeformat = f"{dateformat} {timeformat}"
+
+def to_local_datetime(timestamp, tz):
+    timezone = pytz.timezone(tz)
+    return dt.datetime.fromtimestamp(timestamp, tz=timezone)
+
+
+def time_format(timestamp, tz):
+    return to_local_datetime(timestamp, tz).strftime(timeformat)
+
+
+def date_format(timestamp, tz):
+    return to_local_datetime(timestamp, tz).strftime(dateformat)
+
+
+def datetime_format(timestamp, tz):
+    return to_local_datetime(timestamp, tz).strftime(datetimeformat)
+
+
+api.jinja_env.filters['timefilter'] = time_format
+api.jinja_env.filters['datefilter'] = date_format
+api.jinja_env.filters['datetimefilter'] = datetime_format
 
 
 @api.route('/')
 def index(req, resp):
     resp.text = api.template('index.html')
+
+
+@api.route('/healthcheck')
+def healthcheck(req, resp):
+    resp.text = "all good"
 
 
 @api.route('/weather')
@@ -32,11 +57,11 @@ def weather(req, resp):
     lang = env.str('DARKSKY_LANG', default=None)
     city = env.str('WEATHER_LOCATION', default='San Jose')
     data = {}
-    with forecast(env.str('DARKSKY_KEY'), *location,
-                  units=units, lang=lang) as weather:
-        resp.text = api.template(
-            'weather.html', weather=weather, city=city, celsius=celsius)
+    w = forecast(env.str('DARKSKY_KEY'), *location,
+                  units=units, lang=lang)
+    resp.text = api.template(
+        'weather.html', weather=w, city=city, celsius=celsius)
 
 
 if __name__ == '__main__':
-    api.run(debug=env.bool('DEBUG', default=False))
+    api.run()
