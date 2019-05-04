a74ad593a1e69d6f6d2024f000d090481ac3a046
Paul Chubatyy <xobb@citylance.biz>
2019-05-04 15:26:34 -0700

    Google Calendar api wip

--- app/api.py
+++ app/api.py
@@ -1,10 +1,15 @@
 import sys
 import logging
 import responder
+import os
+import pickle
 from envparse import env
 from darksky import forecast
 import datetime as dt
 import pytz
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
 env.read_envfile()
@@ -30,9 +35,14 @@ h2.setLevel(logging.WARNING)
 logger.addHandler(h1)
 logger.addHandler(h2)
 
+GOOGLE_CREDS_STORE = 'data/token.pickle'
+
 api = responder.API(
     debug=DEBUG,
-    secret_key=env.str('SECRET_KEY')
+    secret_key=env.str('SECRET_KEY'),
+    static_dir='static',
+    templates_dir='templates',
+    title='WunderFridge'
 )
 
 if DEBUG:
@@ -45,20 +55,23 @@ datetimeformat = f"{dateformat} {timeformat}"
 
 logger.info(f"local time in {local_tz}: {dt.datetime.now(tz=local_tz)}")
 
-def to_local_datetime(timestamp, tz):
-    timezone = pytz.timezone(tz)
+def to_local_datetime(timestamp, tz=None):
+    if tz is None:
+        timezone = local_tz
+    else:
+        timezone = pytz.timezone(tz)
     return dt.datetime.fromtimestamp(timestamp, tz=timezone)
 
 
-def time_format(timestamp, tz):
+def time_format(timestamp, tz=None):
     return to_local_datetime(timestamp, tz).strftime(timeformat)
 
 
-def date_format(timestamp, tz):
+def date_format(timestamp, tz=None):
     return to_local_datetime(timestamp, tz).strftime(dateformat)
 
 
-def datetime_format(timestamp, tz):
+def datetime_format(timestamp, tz=None):
     return to_local_datetime(timestamp, tz).strftime(datetimeformat)
 
 
@@ -83,7 +96,7 @@ logger.info("Added jinja filters")
 @api.route('/')
 def index(req, resp):
     now = dt.datetime.now(tz=local_tz)
-    resp.text = api.template('index.html', now=now.strftime('%Y-%m-%dT%H:%M:%SZ'))
+    resp.html = api.template('index.html', now=now.strftime('%Y-%m-%dT%H:%M:%SZ'))
 
 
 @api.route('/healthcheck')
@@ -99,18 +112,52 @@ def weather(req, resp):
     data = {}
     w = forecast(
         env.str('DARKSKY_KEY'), *location, units=weather_units, lang=lang)
-    resp.text = api.template(
-        'weather.html', weather=w, city=city)
+    resp.html = api.template('weather.html', weather=w, city=city)
 
 
 @api.route('/wunderlist')
 def wunderlist(req, resp):
     todos = []
-    resp.text = api.template('wunderlist.html', todos=todos)
+    resp.html = api.template('wunderlist.html', todos=todos)
+
 
 @api.route('/calendar')
 def calendar(req, resp):
-    pass
+    events_amount = env.int('GOOGLE_CALENDAR_EVENT_COUNT', default=20)
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.isfile(GOOGLE_CREDS_STORE):
+        logger.info("Google credentials have been found.")
+        with open(GOOGLE_CREDS_STORE, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, fail miserably.
+    if not creds:
+        logger.error("Failed to deserialize the credentials")
+        resp.html = api.template('calendar/error.html')
+        return
+    if not creds.valid:
+        logger.warning("Credentials not valid")
+        if not creds.expired or not creds.refresh_token:
+            resp.html = api.template('calendar/error.html')
+            return
+        logger.info("Attempting to renew credentials")
+        creds.refresh(Request())
+        with open(GOOGLE_CREDS_STORE, 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('calendar', 'v3', credentials=creds)
+
+    now = dt.datetime.now(tz=local_tz)
+
+    events_result = service.events().list(
+        calendarId='primary',
+        timeMin=now.isoformat(),
+        maxResults=events_amount,
+        singleEvents=True,
+        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    resp.html = api.template('calendar/events.html', events=events, now=now)
 
 
 if __name__ == '__main__':
--- app/googleevents.py
+++ app/googleevents.py
@@ -18,8 +18,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('data/token.pickle'):
+        with open('data/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -30,7 +30,7 @@ def main():
                 'credentials.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('data/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
